54e85946e4dff101a8e1d29def6d6706f3b710c7
Meriem Soubih <soubih.meriem@gmail.com>
2024-07-18 11:08:57 +0100

    added token and user models

---
+++ models/token.model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const TokenSchema = new mongoose.Schema({
+    token: {
+        type: String,
+        required: true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    expiresAt: {
+        type: Date,
+        required: true
+    }
+});
+
+const Token = mongoose.model("Token", TokenSchema);
+module.exports = Token;
---
+++ models/user.model.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    imageURL: {
+        type: String,
+        default: 'https://www.pinterest.com/pin/star-instagram-pfp-default--318700111146556309/'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    },
+    deletedAt: {
+        type: Date
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    }
+});
+
+UserSchema.pre('save', async function (next) {
+    const user = this;
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+    next();
+});
+
+UserSchema.methods.comparePassword = function (candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;

00199013663e5acd69bb171597a73c0121bc5e68
Meriem Soubih <soubih.meriem@gmail.com>
2024-07-19 19:49:55 +0100

    updated server code for socketIO use

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const socketIo = require('socket.io');
+const connectDB = require('./config/mongodb');
 const cors = require('cors');
 require('dotenv').config();
 const { swaggerUi, specs } = require("./docs/swagger");
 const authRouter = require('./routes/auth.routes');
+// const userRouter = require('./routes/user.routes');
+// const chatRouter = require('./routes/chat.routes');
+const socketHelper = require('./helpers/socket.helpers');
 
-const app = express();
+
 const PORT = process.env.PORT || 3000;
+const app = express();
 
 app.use(express.json());
 app.use(cors());
-
-const connectDB = () => mongoose.connect(process.env.MONGODB_URI);
 
 app.get('/', (req, res) => {
   res.send("Welcome to Nexify, a Chat App API.");
@@ -20,12 +23,34 @@ app.get('/', (req, res) => {
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 app.use('/api', authRouter);
+// app.use('/api', userRouter);
+// app.use('/api', chatRouter);
 
-connectDB()
-.then(() => {
-    console.log("connected to database");
-    app.listen(PORT, () => {
-        console.log(`server running in http://localhost:${PORT}`);
-    });
-})
-  .catch((err) => console.log(err));
+connectDB();
+
+const server = app.listen(PORT, () => {
+    console.log(`server running in http://localhost:${PORT}`);
+});
+
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+  console.log(`New client connected: ${socket.id}`);
+
+  socket.on('joinRoom', (chatRoomId) => {
+    socketHelper.joinChatRoom(socket, chatRoomId);
+  });
+
+  socket.on('leaveRoom', (chatRoomId) => {
+    socketHelper.leaveChatRoom(socket, chatRoomId);
+  });
+
+  socket.on('sendMessage', async (data) => {
+    const { chatRoomId, userId, content } = data;
+    await socketHelper.broadcastMessage(io, chatRoomId, userId, content);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});

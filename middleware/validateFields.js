f733ec8f6976d0de9347ab78abdb1f3148b2f1b4
Meriem Soubih <soubih.meriem@gmail.com>
2024-07-20 01:06:12 +0100

    added rules for chatrooms and messages

--- middleware/validateFields.js
+++ middleware/validateFields.js
@@ -4,11 +4,24 @@ const registerRules = [
     check('username').not().isEmpty().withMessage('Username is required'),
     check('email').isEmail().withMessage('Must be a valid email address'),
     check('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+    check('imageURL').optional().isURL().withMessage('Must be a valid URL'),
 ];
 
 const loginRules = [
     check('email').isEmail().withMessage('Must be a valid email address'),
     check('password').not().isEmpty().withMessage('Password is required'),
+];
+
+const createChatRoomRules = [
+    check('name').not().isEmpty().withMessage('Chat room name is required'),
+    check('userIds').isArray().withMessage('User IDs must be an array').notEmpty().withMessage('At least one user is required'),
+    check('userIds.*').isMongoId().withMessage('Each user ID must be a valid MongoDB ID'),
+];
+
+const sendMessageRules = [
+    check('chatRoomId').isMongoId().withMessage('Chat room ID must be a valid MongoDB ID'),
+    check('userId').isMongoId().withMessage('User ID must be a valid MongoDB ID'),
+    check('content').not().isEmpty().withMessage('Message content is required'),
 ];
 
 const validate = (req, res, next) => {
@@ -22,5 +35,7 @@ const validate = (req, res, next) => {
 module.exports = {
     registerRules,
     loginRules,
+    createChatRoomRules,
+    sendMessageRules,
     validate,
 };

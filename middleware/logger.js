1410bd752f2be22d4139280748f90432052e8b15
Meriem Soubih <soubih.meriem@gmail.com>
2024-08-08 11:30:23 +0100

    added request time in logger

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,6 +1,13 @@
-module.exports.logger = (req, res, next) => {
-    console.log(
-      `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`
-    );
-    next();
-  };
+const logger = (req, res, next) => {
+  const start = process.hrtime();
+
+  res.on('finish', () => {
+    const [seconds, nanoseconds] = process.hrtime(start);
+    const milliseconds = (seconds * 1000 + nanoseconds / 1e6).toFixed(3);
+    console.log(`${new Date().toISOString()} - ${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl} - ${res.statusCode} - ${milliseconds}ms`);
+  });
+
+  next();
+};
+
+module.exports.logger = logger;
